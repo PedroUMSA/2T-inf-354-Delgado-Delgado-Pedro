86cfffdc18756057eb6fd31e2bf0ecd3d8dc44d6
PedroUMSA <pdelgado3@umsa.bo>
2020-12-22 10:09:40 -0400

    Delgado Pedro

--- Ejercicio 4/red_neuronal_XOR.py
+++ Ejercicio 4/red_neuronal_XOR.py
@@ -1,12 +1,5 @@
 import numpy as np
- 
-####
-# Vamos a crear una pequeña red neuronal que compute
-# los valores de la tabla XOR
-####
- 
 
- 
 ##Primera parte, generemos un input que sea una matriz
 ## Matriz de 2x4
 ## 0 0
@@ -26,45 +19,26 @@ EXPECTED_RESULT = np.array([[0,1,1,0]]).T # queremos en formato columna
 print ("EXPECTED_RESULT:\n", EXPECTED_RESULT)
 ## Resultado: la tabla de verdad de la operación XOR
 print ("RESULT:\n", np.append(INPUT_X,EXPECTED_RESULT, axis=1))
- 
-## Para aprender, la red neuronal debe computar su predicción
-## calcular el error, corregir los pesos y volver a computar
-## Como tenemos DOS inputs y un OUTPUT, lo que buscamos es conectar
-## cada INPUT con el OUTPUT, cada INPUT debe tener un peso, es decir
-## cada INPUT modifica MAS o MENOS el output segun su peso.
- 
-# Esta es la función de activacion
-# que nos permite modelar problemas NO lineales
-# Esta mapea cualquier valor a entre 0 y 1
-# Aprovechamos la misma función para devolver su derivada, luego lo explicamos.
+
 def sigmoid(x, deriv=False):
     if deriv:
         return x*(1-x)
     return 1/(1+np.exp(-x))
  
-#Seteamos una SEED que nos ayudarà a que los numeros esten
-# distribuidos de forma random, pero siempre igual para entender
-# en qué afecta los cambios que realizamos
 np.random.seed(0)
  
-# Inizializamos las CONEXIONES con una media a 0
-# Esta es la matriz que irà "aprendiendo"
-# La matriz resultando es de 2 x 1 ya que tenemos dos INPUTS y
-# un OUTPUT
+
 SYN0 = 2*np.random.random((2,1)) - 1
  
 # Vamos a preparar iteraciones para aprender
 for i in range(20000):
-    # Primero, computamos con nuestra red, a este paso lo llamamos FORWARD
-    # que resultado tenemos con los pesos
-    # inicializados de forma random,
+
     l0 = INPUT_X
-    #Multiplicamos los INPUTS con las CONEXIONES
+
     l1 = sigmoid(np.dot(l0, SYN0))
-    #Computamos el error
+
     l1_error = EXPECTED_RESULT - l1
-    #Calculamos la derivada multiplicada por error
-    #para computar un cambio a realizar en los cambios
+
     l1_delta = l1_error * sigmoid(l1, True)
  
     SYN0 += np.dot(l0.T, l1_delta)
